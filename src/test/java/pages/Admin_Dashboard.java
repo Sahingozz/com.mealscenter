c63a8c64fb6d3a599d03031dccdde2f93eadc20b
Sahingozz <zafersahingoz@gmail.com>
2023-03-24 18:46:09 +0100

    US_038_TC_02_03_04_05_06 added

--- src/test/java/pages/Admin_Dashboard.java
+++ src/test/java/pages/Admin_Dashboard.java
@@ -32,4 +32,112 @@ public class Admin_Dashboard {
     public WebElement adminAllOrderLink;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Admin Dashboard All Order Text on the list page
+    @FindBy (xpath = "//h5[text()='All Orders']")
+    public WebElement allOrderListText;
+
+    // Admin Dashboard All Order -> 'Cancel' Text
+    @FindBy (xpath = "//p[text()='Cancel']")
+    public WebElement cancelledOrderText;
+
+    // Admin Dashboard All Order -> cancelled order 'Number'
+    @FindBy (xpath = "(//h5[@class='m-0'])[2]")
+    public WebElement cancelledOrderNumber;
+
+    // Admin Dashboard All Order -> 'Total Refund' Text
+    @FindBy (xpath = "//p[text()='Total refund']")
+    public WebElement totalRefundText ;
+
+    // Admin Dashboard All Order -> total Refund 'Number'
+    @FindBy (xpath = "(//h5[@class='m-0'])[3]")
+    public WebElement totalRefundNumber;
+
+    // Admin Dashboard All Order -> 'Total Orders' Text
+    @FindBy (xpath = "//p[text()='Total Orders']")
+    public WebElement totalValueOfOrdersText ;
+
+    // Admin Dashboard All Order -> total orders 'Value'
+    @FindBy (xpath = "(//h5[@class='m-0'])[4]")
+    public WebElement totalValueOfOrders;
+
+    //Admin Dashboard All order -> start-date-end-date
+    @FindBy (xpath = "//input[@placeholder='Start date -- End date']")
+    public WebElement ordersStartEndDate ;
+
+    //Admin Dashboard All Order -> date search result element
+
+    @FindBy (xpath = "//div[@class='dataTables_info']")
+    public WebElement searchResults ;
+
 }
