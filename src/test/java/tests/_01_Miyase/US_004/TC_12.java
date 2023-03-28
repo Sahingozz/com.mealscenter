6dd762aa8b981a674bb0d86325a5e9a5398a92f4
Miyase Colak <colak.miyase44@gmial.com>
2023-03-28 18:26:24 +0200

    US_004 update

--- src/test/java/tests/_01_Miyase/US_004/TC_12.java
+++ src/test/java/tests/_01_Miyase/US_004/TC_12.java
@@ -8,23 +8,30 @@ import pages.Homepage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+import utilities.TestBaseReport;
 
-public class TC_12 {
+public class TC_12 extends TestBaseReport {
+    /**
+     * Open your browser to access the website
+     * -Enter the URL "https://qa.mealscenter.com" into your browser's address bar
+     * -Go to Url on your browser to confirm the entered URL
+     * -Scroll down towards the footer section of the page
+     * -Confirm that BlogList in the footer section is visible
+     * -Close the page
+     */
 
     Homepage homepage = new Homepage();
 
     @Test
     public void testMealCenterFooterBlogList() {
         WebDriver driver = Driver.getDriver();
-        try {
-            JavascriptExecutor jse = (JavascriptExecutor) driver;
-            driver.get(ConfigReader.getProperty("homePageUrl"));
-            jse.executeScript("window.scrollBy(0,5000)");
-            ReusableMethods.wait(2);
-            homepage.homePageCookiesDecline.click();
-            Assert.assertTrue(homepage.homePageFooterBlogList.isDisplayed());
-        } finally {
-            driver.quit();
-        }
+
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        driver.get(ConfigReader.getProperty("homePageUrl"));
+        jse.executeScript("window.scrollBy(0,5000)");
+        ReusableMethods.wait(2);
+        homepage.homePageCookiesDecline.click();
+        Assert.assertTrue(Homepage.homePageFooterBlogList.isDisplayed());
+
     }
 }
--- src/test/java/tests/_01_Miyase/US_004/TC_13.java
+++ src/test/java/tests/_01_Miyase/US_004/TC_13.java
@@ -8,25 +8,34 @@ import pages.Homepage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+import utilities.TestBaseReport;
 
-public class TC_13 {
+public class TC_13 extends TestBaseReport {
+    /**
+     * Open your browser to access the website
+     * -Enter the URL "https://qa.mealscenter.com" into your browser's address bar
+     * -Go to Url on your browser to confirm the entered URL
+     * -Scroll down towards the footer section of the page
+     * -Confirm that Blog Discover Link in the footer section is visible
+     * -Confirm that Blog Discover Link in the footer section is active by clicking on the function
+     * -Close the page
+     */
+
 
     Homepage homepage = new Homepage();
 
     @Test
     public void testMealFooterBlogDiscoveryLink() {
         WebDriver driver = Driver.getDriver();
-        try {
-            JavascriptExecutor jse = (JavascriptExecutor) driver;
-            driver.get(ConfigReader.getProperty("homePageUrl"));
-            jse.executeScript("window.scrollBy(0,5000)");
-            ReusableMethods.wait(2);
-            homepage.homePageCookiesDecline.click();
-            Assert.assertTrue(homepage.homePageFooterBlogDiscoverLink.isDisplayed());
-            homepage.homePageFooterBlogDiscoverLink.click();
-            Assert.assertEquals(Driver.getDriver().getCurrentUrl(), "https://qa.mealscenter.com/discover-the-best-vegetarian-food-delivery-options-in-your-area");
-        } finally {
-            driver.quit();
-        }
+
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        driver.get(ConfigReader.getProperty("homePageUrl"));
+        jse.executeScript("window.scrollBy(0,5000)");
+        ReusableMethods.wait(2);
+        homepage.homePageCookiesDecline.click();
+        Assert.assertTrue(Homepage.homePageFooterBlogDiscoverLink.isDisplayed());
+        Homepage.homePageFooterBlogDiscoverLink.click();
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(), "https://qa.mealscenter.com/discover-the-best-vegetarian-food-delivery-options-in-your-area");
+
     }
 }
