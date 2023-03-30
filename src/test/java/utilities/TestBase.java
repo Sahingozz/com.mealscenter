3804ba314f644fbaec997c316f26984b8d5f2b3d
Sahingozz <zafersahingoz@gmail.com>
2023-03-30 16:00:06 +0200

    2 reports html deleted

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,29 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TestBase {
+    public WebDriver driver;
+
+    @Before
+    public void setup(){
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @After
+    public void teardown(){
+        Driver.quitDriver();
+    }
+
+        }
+
